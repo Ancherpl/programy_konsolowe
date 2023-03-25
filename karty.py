b6b80b969a17fcc3960634a52a400bff6dc93e0b
Daniel Mielech <117686025+Ancherpl@users.noreply.github.com>
2023-03-25 17:03:48 +0100

    Create karty.py

---
+++ karty.py
@@ -0,0 +1,10 @@
+"""
+Napisz program którego zadaniem bedzie sortowanie kart.
+Poprzez karty rozumiemy znaki w postaci cyfr 2,3, .. 9
+oraz litery J,Q,K,A. Program ma za zadanie wczytac ciąg
+znaków oznaczających karty trzymane w ręku oraz wypisać
+ten ciąg w postaci posortowanej od najsłabszej do 
+najsilniejszej karty.
+"""
+
+
